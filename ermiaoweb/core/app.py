0a03a0773ed25b2fe5343f178078786f06558896
ding-hai <dhairoot@gmail.com>
2017-10-03 21:16:07 +0800

    分离工具方法

--- ermiaoweb/core/app.py
+++ ermiaoweb/core/app.py
@@ -4,6 +4,7 @@
 import functools, json
 from ermiaoweb.core import http
 from wsgiref.simple_server import make_server
+from ermiaoweb.utils.tools import *
 
 route_mappings = {}
 middleware_mapping = {http.MiddlewareType.Request.name: {}, http.MiddlewareType.Response.name: {}}
@@ -21,6 +22,7 @@ class App(object):
         # 调用后置中间件
         # 将设置的响应数据转化为原生的http响应
 
+
         def application(environ, start_response):
             # handler = find_matched_handler(environ)
             # before_middleware_list = find_before_middlewares(environ)
@@ -37,9 +39,11 @@ class App(object):
             # bytes = generate_response(response)
             # bytes = [str(environ).encode("utf8")]
 
-            handler = find_matched_handler(environ)
-            request_middleware_list = find_matched_middlewares(environ, http_type=http.MiddlewareType.Request)
-            response_middleware_list = find_matched_middlewares(environ, http_type=http.MiddlewareType.Response)
+            handler = find_matched_handler(environ, route_mappings)
+            request_middleware_list = find_matched_middleware_list(environ, middleware_mapping,
+                                                                   http_type=http.MiddlewareType.Request)
+            response_middleware_list = find_matched_middleware_list(environ, middleware_mapping,
+                                                                    http_type=http.MiddlewareType.Response)
 
             request = parse_request_data(environ)
             if not len(request_middleware_list) == 0:
@@ -62,109 +66,11 @@ class App(object):
             start_response('200 OK', [('Content-Type', 'text/html')])
             return bytes
 
-        def parse_request_data(environ):
-            request = http.Request()
-            # request method
-            request_method = environ['REQUEST_METHOD']
-            request.parse_request_method(request_method)
-
-            # url
-            url = environ["PATH_INFO"]
-            request.url = url
-
-            # headers
-            # TODO
-            content_type = environ.get('CONTENT_TYPE', '')
-            request.headers["CONTENT_TYPE"] = content_type
-
-            # cookies
-            cookies_string = environ.get('HTTP_COOKIE', '')
-            request.parse_cookies(cookies_string)
-
-            # url 上的数据
-            query_string = environ.get('QUERY_STRING', '')
-            request.parse_query_string(query_string)
-
-            # request body
-            content_length = 0
-            if not environ.get('CONTENT_LENGTH') == "":
-                content_length = int(environ.get('CONTENT_LENGTH'))
-            request.content_length = content_length
-            wsgi_input = environ.get('wsgi.input')
-            request.parse_request_body(wsgi_input)
-
-            return request
-
-        def generate_response(response):
-            if isinstance(response, str):
-                return response
-            if isinstance(response, (dict, list,)):
-                return json.dumps(response)
-            if isinstance(response, object):
-                return json.dumps(response.__dict__)
-
-        # 根据url以及route_mapping找出匹配的function
-        def find_matched_handler(environ):
-            url = environ.get('PATH_INFO', '/')
-            method = environ.get("REQUEST_METHOD", "GET")
-            # print(method, url)
-            if method == 'GET':
-                method = http.HttpMethod.GET
-            elif method == 'POST':
-                method = http.HttpMethod.POST
-            else:
-                pass
-
-            try:
-                handler = route_mappings[url][method.name]
-                # print(handler.__name__)
-                return handler
-
-            except KeyError:
-                print("no matched handler")
-
-        def find_matched_middlewares(environ, http_type=http.MiddlewareType.Request):
-            url = environ.get('PATH_INFO', '/')
-            method = environ.get("REQUEST_METHOD", "GET")
-            # print(method, url)
-            if method == 'GET':
-                method = http.HttpMethod.GET
-            elif method == 'POST':
-                method = http.HttpMethod.POST
-            else:
-                pass
-
-            try:
-                middlewares = middleware_mapping[http_type.name][url][method.name]
-                print(middlewares)
-                return middlewares
-            except KeyError:
-                print("no matched middleware")
-                return []
-
-        def parse_cookies(cookies_string):
-            cookies_dict = {}
-            cookies_array = cookies_string.split(";")
-            for cookie in cookies_array:
-                name_value_pairs = cookie.split("=")
-                name = name_value_pairs[0]
-                value = name_value_pairs[1]
-                if name not in cookies_dict.keys():
-                    cookies_dict[name] = value
-            return cookies_dict
-
         httpd = make_server('', 8000, application)
         httpd.serve_forever()
 
 
 def route(url, *, methods=(http.HttpMethod.GET,), name=None):
-    """
-    :param url:
-    :param methods:
-    :return:
-    :function:添加url和处理函数到route_mapping
-    """
-
     def wrapper(fun):
         # 注册路由
         method_and_func = {}
---
+++ ermiaoweb/utils/tools.py
@@ -0,0 +1,89 @@
+# coding:utf8
+# author:dinghai
+# created on 2017-10-03 21:06
+import json
+from ermiaoweb.core import http
+
+
+def parse_request_data(environ):
+    request = http.Request()
+    # request method
+    request_method = environ['REQUEST_METHOD']
+    request.parse_request_method(request_method)
+
+    # url
+    url = environ["PATH_INFO"]
+    request.url = url
+
+    # headers
+    # TODO
+    content_type = environ.get('CONTENT_TYPE', '')
+    request.headers["CONTENT_TYPE"] = content_type
+
+    # cookies
+    cookies_string = environ.get('HTTP_COOKIE', '')
+    request.parse_cookies(cookies_string)
+
+    # url 上的数据
+    query_string = environ.get('QUERY_STRING', '')
+    request.parse_query_string(query_string)
+
+    # request body
+    content_length = 0
+    if not environ.get('CONTENT_LENGTH') == "":
+        content_length = int(environ.get('CONTENT_LENGTH'))
+    request.content_length = content_length
+    wsgi_input = environ.get('wsgi.input')
+    request.parse_request_body(wsgi_input)
+
+    return request
+
+
+def generate_response(response):
+    if isinstance(response, str):
+        return response
+    if isinstance(response, (dict, list,)):
+        return json.dumps(response)
+    if isinstance(response, object):
+        return json.dumps(response.__dict__)
+
+
+# 根据url以及route_mapping找出匹配的function
+def find_matched_handler(environ, route_mappings):
+    url = environ.get('PATH_INFO', '/')
+    method = environ.get("REQUEST_METHOD", "GET")
+    # print(method, url)
+    if method == 'GET':
+        method = http.HttpMethod.GET
+    elif method == 'POST':
+        method = http.HttpMethod.POST
+    else:
+        pass
+
+    try:
+        handler = route_mappings[url][method.name]
+        # print(handler.__name__)
+        return handler
+
+    except KeyError:
+        print("no matched handler")
+
+
+def find_matched_middleware_list(environ, middleware_mapping, http_type=http.MiddlewareType.Request):
+    url = environ.get('PATH_INFO', '/')
+    method = environ.get("REQUEST_METHOD", "GET")
+    # print(method, url)
+    if method == 'GET':
+        method = http.HttpMethod.GET
+    elif method == 'POST':
+        method = http.HttpMethod.POST
+    else:
+        pass
+
+    try:
+        middleware_list = middleware_mapping[http_type.name][url][method.name]
+        # print(middleware_list)
+        return middleware_list
+    except KeyError:
+        print("no matched middleware")
+        return []
